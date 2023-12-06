58bf39aab7d2b7f4afe0f1b5fbca3fb35f99d194
CesGalaxy <cesarpro90@hotmail.com>
2023-12-06 16:49:12 +0100

    Add authentication

--- src/app/auth/auth-button.tsx
+++ src/app/auth/auth-button.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {Session, createClientComponentClient} from "@supabase/auth-helpers-nextjs";
 import {GitHubIcon} from "@/theme/icons";
+import { useEffect, useState } from "react";
 
 export function AuthButton() {
+    const [session, setSession] = useState<Session | null>(null);
+
     const supabase = createClientComponentClient();
 
     const handleSignIn = async () => {
@@ -19,18 +22,24 @@ export function AuthButton() {
         await supabase.auth.signOut();
     }
 
-    return (
-        <div>
-            <button type="button"
-                    className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2"
-                    onClick={handleSignIn}>
-                <GitHubIcon/>
-                Sign in with Github
-            </button>
-            <button type="button"
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                Sign Out
-            </button>
-        </div>
+    useEffect(() => {
+        const getSession = async () => {
+            const { data } = await supabase.auth.getSession();
+            setSession(data.session);
+        }
+    }, []);
+
+    return session ? (
+        <button type="button"
+                className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mb-2"
+                onClick={handleSignIn}>
+            <GitHubIcon/>
+            Sign in with Github
+        </button>
+    ) : (
+        <button type="button"
+                className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+            Sign Out
+        </button>
     )
 }
--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,5 +1,17 @@
-import {NextRequest} from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
+
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
+    const requestUrl = new URL(req.nextUrl);
+    const code = requestUrl.searchParams.get('code');
 
+    if (!code) return NextResponse.redirect('/');
+
+    const supabase = createRouteHandlerClient({cookies});
+    await supabase.auth.exchangeCodeForSession(code);
+
+    return NextResponse.redirect(requestUrl.origin);
 }
